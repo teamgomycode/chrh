53d64d7aa4ec8f14aea8014f8a655ce38b11014b
hakimnouira <nouira.hakimo@gmail.com>
2022-06-10 12:22:12 +0100

    rbtet bin html ow java --hakim

---
+++ html/file.js
@@ -0,0 +1,34 @@
+
+
+
+function sasie(a){
+
+
+    document.f.T1.value= document.f.T1.value+a
+
+
+}
+
+function calcul(){
+
+
+    document.f.T2.value = document.f.T1.value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    document.f.T1.value =""
+
+}
+
